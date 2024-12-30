62cffb06bab9233861f813dadcab734e15f38cc9
Nick Volf <boston.nick@gmail.com>
2024-12-30 19:08:51 +0900

    removed loice, added mini notification

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -4,18 +4,18 @@ return {
 		config = function()
 			local statusline = require 'mini.statusline'
 			statusline.setup {use_icons = true }
-			local minisurround = require 'mini.surround'
-			minisurround.setup {}
-            require 'mini.pairs'.setup {}
-            require 'mini.comment'.setup {}
-            require 'mini.cursorword'.setup {}
+            require 'mini.ai'.setup {}
             require 'mini.animate'.setup {}
-            require "mini.surround".setup {}
-            require 'mini.starter'.setup {}
-            require 'mini.move'.setup {}
-            require 'mini.tabline'.setup {}
             require 'mini.bracketed'.setup {}
             require 'mini.clue'.setup {}
+            require 'mini.comment'.setup {}
+            require 'mini.cursorword'.setup {}
+            require 'mini.move'.setup {}
+            require 'mini.notify'.setup {}
+            require 'mini.pairs'.setup {}
+            require 'mini.starter'.setup {}
+			require 'mini.surround'.setup {}
+            require 'mini.tabline'.setup {}
 		end
 	},
 }
